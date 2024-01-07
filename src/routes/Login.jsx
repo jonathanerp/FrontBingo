3db1f9b3cb0d2d3d90ff3af235dd9bac3671a209
jonathan <joesrape@hotmail.com>
2024-01-07 07:55:29 -0500

    agregando inputs a las rutas de registro y login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,15 +3,33 @@ import { useState } from "react"
 const Login = () => {
 
     const [nombre, setNombre] = useState("")
+    const [contraseña, setContraseña] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        alert("Iniciaste sesion");
+    }
 
     return (
-        <form>
-            <h1>Registro</h1>
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
             <input
                 type='text'
                 placeholder='Ingresar Nombre'
                 value={nombre}
                 onChange={e =>setNombre(e.target.value)}
+            />
+
+            <input
+                type='text'
+                placeholder='Ingresar Email'
+                value={contraseña}
+                onChange={e =>setContraseña(e.target.value)}
+            />
+        
+            <input
+                type="submit"
+                value="Enviar datos"
             />
         </form>
     )
--- src/routes/Registro.jsx
+++ src/routes/Registro.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
 
 const Registro = () => {
+
+    const [nombre, setNombre] = useState("")
+    const [contraseña, setContraseña] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        alert("Informacion guardada");
+    }
+
     return (
-        <div>Registro</div>
+        <form onSubmit={handleSubmit}>
+            <h1>Registro</h1>
+            <input
+                type='text'
+                placeholder='Ingresar Nombre'
+                value={nombre}
+                onChange={e =>setNombre(e.target.value)}
+            />
+
+            <input
+                type='text'
+                placeholder='Ingresar Email'
+                value={contraseña}
+                onChange={e =>setContraseña(e.target.value)}
+            />
+        
+            <input
+                type="submit"
+                value="Enviar datos"
+            />
+        </form>
     )
 }
 
